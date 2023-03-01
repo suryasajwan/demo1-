d130f449ffa75e3496fe9be13159b733107419e2
surya <sajwansurya951@gmail.com>
2023-03-01 10:56:03 +0530

    march- 1 LSS  class covered topics

---
+++ second.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+VAR1=123456
+TEST=RVCEMCA
+#DOUBLE QUOTES
+
+echo "this is Double qyute $VAR1  $TEST "
+
+#SINGLE QUOTE
+echo 'this is  single quote demo $VAR1 $TEST'
+
+#REVERSE QUOTE /backtick
+
+echo"this computer /laptop hostname is: `hostname`"
